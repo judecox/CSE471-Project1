df7fac07375a45a7d175a0e79681f45678f83938
Jude <coxjude@msu.edu>
2023-10-26 19:19:15 -0400

    Added some commented-out code to CPiano for the sake of getting some feedback and converting some of the class's format to support use of audio samples instead of direct sine wave generation.

--- Synthie/CPiano.h
+++ Synthie/CPiano.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "CInstrument.h"
 #include "CARSineWave.h"
+//#include "CSample.h"
 
 #include <memory>
 
@@ -17,11 +18,43 @@ public:
 
 	void SetFreq(double f) { m_sinewave->SetFreq(f); }
 	void SetAmplitude(double a) { m_sinewave->SetAmplitude(a); }
+
+	// below code is meant to interact with the CSample class, once/if it is implemented
+	/*
+		void SetSample(CSample* sample) { m_sample = sample; }
+		double Duration() { return m_sample->Duration(); }
+	*/
 	
 	double Duration() { return m_sinewave->Duration(); }
 
 private:
+	// std::unique_ptr<CSample> m_sample;
 	std::unique_ptr<CARSineWave> m_sinewave;
 	double m_time;
+
+
+/*
+	// The CSample class in this case would give the capability to access a set 
+	// Piano audio sample.wav files and set whatever given sample to SetNote in CPiano
+
+	#pragma once
+
+	#include <string>
+	#include <vector>
+
+	class CSample
+	{
+	public:
+		CSample(const std::string& filePath);
+
+		double GetSampleRate() const { return m_sampleRate; }
+		double GetDuration() const { return m_duration; }
+		const std::vector<double>& GetSamples() const { return m_samples; }
+
+	private:
+		double m_sampleRate;
+		double m_duration;
+		std::vector<double> m_samples;
+	};*/
 };
 
