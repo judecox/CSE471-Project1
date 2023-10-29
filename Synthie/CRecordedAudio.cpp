2eed5b76e674e7aa5524332e07f584938475a6ff
nagy125 <113730949+nagy125@users.noreply.github.com>
2023-10-29 01:21:01 -0400

    Reson Filter (Just for recorded performance)
    Implemented a reson filter that is applied only to a recorded performance, and that can be dynamically updated. (Note: This does not include XML integration yet. Also, the fuzz effect was implemented in a previous version, but I don't think I documented that at the time since I hadn't tested it yet, but it works, too.)

--- Synthie/CRecordedAudio.cpp
+++ Synthie/CRecordedAudio.cpp
@@ -3,7 +3,6 @@
 #include "pch.h"
 #include "CRecordedAudio.h"
 
-
 CRecordedAudio::CRecordedAudio()
 {
 	m_duration = 0;
@@ -19,6 +18,9 @@ CRecordedAudio::~CRecordedAudio()
 void CRecordedAudio::Start()
 {
 	m_elapsed = 0;
+	wrloc = 0;
+	xqueue.resize(m_delay_length);
+	yqueue.resize(m_delay_length);
 }
 
 bool CRecordedAudio::Generate()
@@ -33,27 +35,98 @@ bool CRecordedAudio::Generate()
 	m_wavein.ReadFrame(audio);
 	short test2 = audio[0];
 	
+	//Fuzz
+	if (audio[0] > 32767 * (1.0 - m_fuzz))
+	{
+		audio[0] = 32767 * (1.0 - m_fuzz);
+	}
+	if (audio[0] < 32767 * (-1.0 + m_fuzz))
+	{
+		audio[0] = 32767 * (-1.0 + m_fuzz);
+	}
+
+	if (audio[1] > 32767 * (1.0 - m_fuzz))
+	{
+		audio[1] = 32767 * (1.0 - m_fuzz);
+	}
+	if (audio[1] < 32767 * (-1.0 + m_fuzz))
+	{
+		audio[1] = 32767 * (-1.0 + m_fuzz);
+	}
+
+	audio[0] *= m_amplitude;
+	audio[1] *= m_amplitude;
+
+	if (m_reson_enabled)
+	{
+		float R = 1. - (m_reson_band / 2.);
+
+		float cosine_value = (2. * R * cos(2 * 3.14159265358979323846 * m_reson_freq)) / (1. + (R * R));
+		float A = sin(acos(cosine_value)) * (1. - (R * R)) * m_gain;
+
+
+		wrloc = (wrloc + 2) % m_delay_length;
+		if (wrloc >= m_delay_length)
+		{
+			wrloc -= m_delay_length;
+		}
+
+		//Save X Terms
+		xqueue[wrloc] = audio[0];
+		xqueue[wrloc + 1] = audio[1];
+		audio[0] = 0;
+		audio[1] = 0;
+
+		int rdloc = (wrloc + m_delay_length) % m_delay_length; //Delaylength is int((delay + 0.5)) * 2;
+		if (rdloc >= m_delay_length)
+		{
+			rdloc -= m_delay_length;
+		}
+
+		//Process X Terms
+		audio[0] += (audio[0] / 2 + xqueue[rdloc++] / 2) * A;
+		if (rdloc >= m_delay_length)
+		{
+			rdloc -= m_delay_length;
+		}
+		audio[1] += (audio[1] / 2 + xqueue[rdloc] / 2) * A;
+
+		//Process Y Terms
+		
+		rdloc = (wrloc + m_delay_length - int((1 + 0.5)) * 2) % m_delay_length;
+		audio[0] += (audio[0] / 2 + yqueue[rdloc++] / 2) * (2. * R * cosine_value);
+		if (rdloc >= m_delay_length)
+		{
+			rdloc -= m_delay_length;
+		}
+		audio[1] += (audio[1] / 2 + yqueue[rdloc] / 2) * (2. * R * cosine_value);
+		
+
+		rdloc = (wrloc + m_delay_length - int((2 + 0.5)) * 2) % m_delay_length;
+		audio[0] += (audio[0] / 2 + yqueue[rdloc++] / 2) * ( - 1 * R * R);
+		if (rdloc >= m_delay_length)
+		{
+			rdloc -= m_delay_length;
+		}
+		audio[1] += (audio[1] / 2 + yqueue[rdloc] / 2) * ( - 1 * R * R);
+		
+
+		//Write Y Terms
+		yqueue[wrloc] = audio[0];
+		yqueue[wrloc + 1] = audio[1];
+	}
+
 	m_frame[0] = (double)audio[0] / (double)32767; //Conversion between short and float
-	if (m_frame[0] > (1.0 - m_fuzz))
-	{
-		m_frame[0] = (1.0 - m_fuzz);
-	}
-	if (m_frame[0] < (-1.0 + m_fuzz))
-	{
-		m_frame[0] = (-1.0 + m_fuzz);
-	}
-	m_frame[0] *= m_amplitude;
+	
+
 
 	m_frame[1] = (double)audio[1] / (double)32767;
-	if (m_frame[1] > (1.0 - m_fuzz))
-	{
-		m_frame[1] = (1.0 - m_fuzz);
-	}
-	if (m_frame[1] < (-1.0 + m_fuzz))
-	{
-		m_frame[1] = (-1.0 + m_fuzz);
-	}
-	m_frame[1] *= m_amplitude;
+	
+
+	//m_frame[0] *= m_amplitude;
+	//m_frame[1] *= m_amplitude;
+
+	
 	//m_time += GetSamplePeriod();
 	m_elapsed++;
 
--- Synthie/CRecordedAudio.h
+++ Synthie/CRecordedAudio.h
@@ -18,7 +18,9 @@ public:
     CWaveformBuffer m_waveformBuffer;
     void SetFuzz(double fuzz) { m_fuzz = fuzz; }
     double FrameValue(int c) { return m_frame[c]; }
-    double SetAmplitude(double amp) { m_amplitude = amp; }
+    void SetAmplitude(double amp) { m_amplitude = amp; }
+    void SetResonFrequency(double freq) { m_reson_freq = freq; }
+    void SetResonBand(double band) { m_reson_band = band; }
     
 private:
     int m_duration; //duration in frames
@@ -28,5 +30,17 @@ private:
     double m_frame[2];
     double m_fuzz = 0.0;
     double m_amplitude = 1.0;
+
+    bool m_reson_enabled = true;
+
+    int m_delay_length = 88202; //Times the number of frames * channels to get queue size
+    double m_delay = 1.0;
+    double m_gain = 10.5;
+    double m_reson_freq = 0.00998;
+    double m_reson_band = 0.015;
+    int wrloc;
+
+    std::vector<double> xqueue;
+    std::vector<double> yqueue;
 };
 
